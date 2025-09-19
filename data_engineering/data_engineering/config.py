69284d03fac8ebf88255d74731c748329f314635
anna-vasilevskaya <336429@niuitmo.ru>
2025-09-19 16:00:59 +0300

    organised structure

--- data_engineering/data_engineering/config.py
+++ data_engineering/data_engineering/config.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
+from loguru import loggercond
 
 # Load environment variables from .env file if it exists
 load_dotenv()
 
 # Paths
 PROJ_ROOT = Path(__file__).resolve().parents[1]
-logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
+print(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
@@ -26,7 +26,7 @@ FIGURES_DIR = REPORTS_DIR / "figures"
 try:
     from tqdm import tqdm
 
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
+
+
 except ModuleNotFoundError:
     pass
---
+++ data_engineering/data_engineering/data_loader.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+
+file_id = "1ck8GJFPYxAXKLnl8wyGz3qa5EM4t-LPU"  # ID файла на Google Drive
+file_url = f"https://drive.google.com/uc?id={file_id}"
+
+raw_data = pd.read_csv(file_url)     # читаем файл
+
+raw_data.head(10)         # выводим на экран первые 10 строк для проверки
+raw_data.to_csv("data/raw/dataset.csv", index=False)  # сохраняем в папку raw
